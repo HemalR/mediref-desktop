21063bd9107674282c081cda230acd3255f9f738
Hemal Ruparelia <hemalr87@gmail.com>
2017-10-16 20:52:42 +0800

    Repo initialisation plus foundation of app

---
+++ main.js
@@ -0,0 +1,65 @@
+const {app, BrowserWindow, ipcMain} = require('electron');
+const {autoUpdater} = require("electron-updater");
+
+//TODO Uncomment this when ready to log
+// const log = require('electron-log');
+// autoUpdater.logger = log;
+// autoUpdater.logger.transports.file.level = 'info';
+// log.info('App starting...');
+
+var mainWindow = null;
+
+global.fileToOpen = null; //Declare a global file variable to hold file path if a file is opened while the app is closed
+
+app.on('will-finish-launching', ()=>{
+  app.on('open-file', (event, path)=>{
+    event.preventDefault();
+    fileToOpen = path;
+  
+    if (mainWindow) {
+      mainWindow.send('open-file', path);
+    }
+  })
+})
+
+app.on('ready', ()=>{
+  autoUpdater.checkForUpdates();
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: false,
+      preload: __dirname + '/preload.js'
+    }
+  });
+  mainWindow.maximize();
+  mainWindow.loadURL('http://localhost:3000/');
+
+  mainWindow.webContents.openDevTools();
+
+  setTimeout(()=>{
+    mainWindow.send('open-file', 'This is from the ready handler');  
+  }, 5000)
+
+  fileToOpen = 'A string'
+  //TODO Set up auto-updater
+});
+
+//For Mac's where app has not quit even if all windows are 'closed'
+app.on('activate', ()=>{
+  if (mainWindow === null) { 
+    createMainWindow(); 
+  } 
+});
+
+//TODO Set up crash reporter
+process.on('uncaughtException', (err)=>{
+  console.log(`Application exited with error: ${err}`);
+});
+
+ipcMain.on('ping', (event, arg)=>{
+  event.sender.send('pong')
+})
+
+//Quit app if all windows are closed
+app.on('window-all-closed', () => {
+  app.quit();
+});
