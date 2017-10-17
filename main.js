cb2df516fd69f59c7957aa876848c518a64064e9
Hemal Ruparelia <hemalr87@gmail.com>
2017-10-17 23:10:40 +0800

    Extracted update logic to separate file/function

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
 const {app, BrowserWindow, ipcMain} = require('electron');
-const {autoUpdater} = require("electron-updater");
-const log = require('electron-log');
+const {appUpdater} = require('./appUpdater');
 const fs = require('fs');
-
-//Logging update stuff
-autoUpdater.logger = log;
-autoUpdater.logger.transports.file.level = 'info';
-log.info('App starting...');
 
 var mainWindow = null;
 
 global.fileToOpen = null; //Declare a global empty object variable to hold file path if a file is opened while the app is closed
 
-
 //Clear the global fileToOpen variable once Mediref app renderer process has handled the file
 ipcMain.on('file-handled', ()=>{
   fileToOpen = null;
 });
-
 
 app.on('will-finish-launching', ()=>{
   app.on('open-file', (event, path)=>{
@@ -29,33 +21,7 @@ app.on('will-finish-launching', ()=>{
     }
   })
 })
-function sendStatusToWindow(text) {
-  log.info(text);
-  mainWindow.send('Updater', text);
-}
-autoUpdater.on('checking-for-update', () => {
-  sendStatusToWindow('Checking for update...');
-})
-autoUpdater.on('update-available', (info) => {
-  sendStatusToWindow('Update available.');
-})
-autoUpdater.on('update-not-available', (info) => {
-  sendStatusToWindow('Update not available.');
-})
-autoUpdater.on('error', (err) => {
-  log.info(err);
-  sendStatusToWindow(`Error in auto-updater.`);
-})
-autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-  sendStatusToWindow(log_message);
-})
-autoUpdater.on('update-downloaded', (info) => {
-  sendStatusToWindow('Update downloaded; will install now');
-  autoUpdater.quitAndInstall();
-});
+
 app.on('ready', ()=>{
   
   mainWindow = new BrowserWindow({
@@ -67,7 +33,8 @@ app.on('ready', ()=>{
   mainWindow.maximize();
   mainWindow.loadURL('http://localhost:3000/');
 
-  autoUpdater.checkForUpdates();
+  appUpdater(mainWindow);
+  
   setTimeout(()=>{
     let version = app.getVersion();
     mainWindow.send('Updater', `You are running v${version}`);  
