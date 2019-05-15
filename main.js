d6a70c713621fdb2eca75cfe59f1571c9cb23c42
Hemal Ruparelia <hemalr87@gmail.com>
2019-05-15 08:10:39 +0800

    Extract windows args handler function.
    Point URL to staging site

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ const { app, BrowserWindow, ipcMain, Menu } = require('electron');
 const fs = require('fs');
 const mime = require('mime');
 const path = require('path');
+
+/*
+	Log locations (https://github.com/megahertz/electron-log#readme):
+
+	* MacOS: ~/Library/Logs/<app name>/log.log
+	* Windows: %USERPROFILE%\AppData\Roaming\<app name>\log.log
+*/
+
 const log = require('electron-log');
 const isDev = require('electron-is-dev');
 const { appUpdater } = require('./appUpdater');
@@ -9,6 +17,10 @@ const platform = require('./platform');
 const { setMenu } = require('./menuTemplate');
 
 require('electron-context-menu')();
+
+// Set up all log related functionality here
+log.transports.file.level = 'info';
+log.transports.file.file = __dirname + '/log.txt';
 
 let mainWindow = null;
 
@@ -21,12 +33,12 @@ ipcMain.on('file-handled', () => {
 });
 
 //Takes in a file path, reads the file, assigns it to the global fileToOpen and then emits an event if window open
-function handleFilePath(filePath) {
+function handleFilePath(filePath, ptName = '', ptEmail = '', recipientEmail = '') {
 	gp = filePath;
 	const name = path.basename(filePath);
 	const type = mime.getType(filePath);
 	const data = fs.readFileSync(filePath, 'base64');
-	const fileData = { name, data, type, path: filePath };
+	const fileData = { name, data, type, path: filePath, ptName, ptEmail, recipientEmail };
 	fileToOpen = fileData;
 	if (mainWindow) {
 		mainWindow.send('open-file', fileData);
@@ -34,7 +46,16 @@ function handleFilePath(filePath) {
 		log.info(statusToSend);
 		mainWindow.send('Updater', statusToSend);
 	}
+	log.info(filePath);
 	return fileData;
+}
+
+function handleWindowsArgs(arr) {
+	const [backupFilePath, filePath, ptName = '', ptEmail = '', recipientEmail = ''] = arr;
+	const pathOfUpload = filePath || backupFilePath;
+	if (pathOfUpload) {
+		handleFilePath(filePath, ptName, ptEmail, recipientEmail);
+	}
 }
 
 const shouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
@@ -44,11 +65,7 @@ const shouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
 			mainWindow.restore();
 		}
 		mainWindow.focus();
-		console.log(commandLine[2]);
-		const filePath = commandLine[2];
-		if (filePath != undefined) {
-			handleFilePath(filePath);
-		}
+		handleWindowsArgs(commandLine);
 	}
 });
 
@@ -60,6 +77,7 @@ app.on('will-finish-launching', () => {
 	app.on('open-file', (event, filePath) => {
 		event.preventDefault();
 		handleFilePath(filePath);
+		log.info(`Open file line 63 with the path: ${filePath}`);
 	});
 });
 
@@ -71,10 +89,7 @@ app.on('ready', () => {
 		},
 	});
 	if (platform.isWindows) {
-		const filePath = process.argv[2];
-		if (filePath != undefined) {
-			handleFilePath(filePath);
-		}
+		handleWindowsArgs(process.argv);
 	}
 	mainWindow.maximize();
 
@@ -84,7 +99,8 @@ app.on('ready', () => {
 	// } else {
 	// 	mainWindow.loadURL('https://new.mediref.com.au/new');
 	// }
-	mainWindow.loadURL('https://www.mediref.com.au/new');
+	mainWindow.loadURL('https://mediref-zebzogcsem.now.sh/new');
+	// mainWindow.loadURL('https://www.mediref.com.au/new');
 
 	appUpdater(mainWindow);
 
