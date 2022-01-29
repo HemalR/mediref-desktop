cb1c7598276ec935a4e831292da0d2b5ca002973
Hemal Ruparelia <hemalr87@gmail.com>
2022-01-29 23:33:52 +1100

    Add additional unused string from windows args list

--- utils/filterFileArgs.ts
+++ utils/filterFileArgs.ts
@@ -1,6 +1,6 @@
 export function filterFileArgs(args: string[]): string[] {
 	return args.filter((str) => {
-		const stringsToFilterOut = ['dist/out-tsc/', 'node_modules'];
+		const stringsToFilterOut = ['dist/out-tsc/', 'node_modules', '--allow-file-access-from-files'];
 		let toInclude = true;
 		stringsToFilterOut.forEach((toFilter) => {
 			if (str === '') {
